13ac07de7c7ef0803dfb3870940a12baf4380b8f
sanjaykv86-coder <sanjaykv86@gmail.com>
2025-08-25 16:41:01 +0530

    asdf

--- my-dotnet-app/src/MyDotNetApp/obj/Debug/net6.0/MyDotNetApp.AssemblyInfo.cs
+++ my-dotnet-app/src/MyDotNetApp/obj/Debug/net6.0/MyDotNetApp.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyDotNetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e0226e4f499e7bfbd9eac29cf90ea1c1ae184a")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc34e9a56da26999da8f137ea5200f7cd99bc613")]
->>>>>>> main
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f1fbe27b213e4cedf756b60b17c360ffa2bf84d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyDotNetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyDotNetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
